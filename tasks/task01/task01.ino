a9caf099c101be93a2949016ff6a9efb2bf49eb0
Uspenskiy.Dmitriy <12gamedevowl21@gmail.com>
2024-04-14 18:31:32 +0400

    Update task 1

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,29 @@
-void setup() {
+int pinsCount = 5;
+int pinsNumbers[] = {3, 5, 6, 9, 10};
+int freqAmount[] = {50, 100, 500, 1000, 10000};
 
+int states[] = {LOW, LOW, LOW, LOW, LOW};
+int prevMicros[] = {0, 0, 0, 0, 0};
+
+
+void setup() {
+  for (int i = 0; i < pinsCount; i++){
+    pinMode(pinsNumbers[i], OUTPUT);
+  }
 }
 
 void loop() {
-
+  int currentMicros = micros();
+  for (int j = 0; j < pinsCount; j++){
+    if (currentMicros - prevMicros[j] >= freqAmount[j]){
+      prevMicros[j] = currentMicros;
+      if (states[j] == LOW){
+        states[j] = HIGH;
+      }
+      else{
+        states[j] = LOW;
+      }
+      digitalWrite(pinsNumbers[j], states[j]);
+    }
+  }
 }
